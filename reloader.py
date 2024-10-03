ace5a75854cf53d07efcbe8a6f56f5e189f1a2e2
仿生猫梦见苦力怕 <3372728115@qq.com>
2024-10-03 10:42:28 +0800

    updated

--- reloader.py
+++ reloader.py
@@ -22,6 +22,10 @@ def gr():
     result = origin.strip().split("。")
     result = [i.strip("\n") for i in result]
     result = [f"<ListItem><Paragraph>{i}。</Paragraph></ListItem>\n" for i in result]
+    links = get_link_txt(str(obj.find('div', class_="weekly-content")))
+    for k, v in links.items():
+        result = [i.replace(k, link_to_xaml((k, v))) if k in i else i for i in result]
+    print(result)
     result.pop()
     return result
 
@@ -36,6 +40,10 @@ def get_link_txt(txt):
             ("https://zh.minecraft.wiki" +
              link[re.search(r'".*?"', link).span()[0]:re.search(r'".*?"', link).span()[1]].strip('"'))
     return links
+
+
+def link_to_xaml(lk):
+    return f'''<Underline><local:MyTextButton EventType="打开网页" EventData="{lk[0]}" Margin="0,0,0,-8">{lk[1]}</local:MyTextButton></Underline>'''
 
 
 def gs():
@@ -61,7 +69,7 @@ def update():
 <ImageBrush ImageSource="{gs()}" Stretch="UniformToFill"/>
 </Border.Background>
 <Image Source="{gs()}" Opacity="0" Stretch="Fill"/>
-</Border>''', content_text)
+</Border>''', content_text, re.S)
         txt = re.sub(r'<sys:String x:Key="datetime">.*?</sys:String>',
                      f'<sys:String x:Key="datetime">最后更新：{now.strftime("%Y-%m-%d")}</sys:String>', txt)
         txt = re.sub(r'<sys:String x:Key="WikiPage">.*?</sys:String>', '<sys:String x:Key="WikiPage">' +
@@ -69,10 +77,14 @@ def update():
                      "</sys:String>", txt)
         txt = re.sub(r'<!-- NewsCard -->.*?<!-- end_NewsCard -->',
                      f'<!-- NewsCard -->{get_news_card()}<!-- end_NewsCard -->', txt, re.S)
-        txt = re.sub(r'<!-- introduction -->.*?<!-- introduction -->',
-                     f)
+        txt = re.sub(r'<!-- introduction -->.*?<!-- end_introduction -->',
+                     f'<!-- introduction -->{gr()[0]}<!-- end_introduction -->', txt, re.S, re.M)
+        txt = re.sub(r'<!-- intro_2 -->.*?<!-- end_intro_2 -->',
+                     f'<!-- intro_2 -->{gr()[1]}<!-- end_intro_2 -->', txt, re.S)
+        txt = re.sub(r'<!-- body -->.*?<!-- end_body -->',
+                     f'<!-- body -->{"".join(gr()[2:])}<!-- end_body -->', txt, re.S)
         print(txt)
         f.write(txt)
 
 
-print(gr())
+update()
