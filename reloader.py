9b968ecf95934c27a6ab8d640aae0c4d3256989e
仿生猫梦见苦力怕 <3372728115@qq.com>
2024-10-09 18:05:03 +0800

    updated

--- reloader.py
+++ reloader.py
@@ -21,11 +21,10 @@ def gr():
     origin = obj.find('div', class_="weekly-content").text
     result = origin.strip().split("。")
     result = [i.strip("\n") for i in result]
-    result = [f"<ListItem><Paragraph>{i}。</Paragraph></ListItem>\n" for i in result]
+    result = [f"<ListItem><Paragraph>{i}。</Paragraph></ListItem>" for i in result]
     links = get_link_txt(str(obj.find('div', class_="weekly-content")))
     for k, v in links.items():
         result = [i.replace(k, link_to_xaml((k, v))) if k in i else i for i in result]
-    print(result)
     result.pop()
     return result
 
@@ -34,16 +33,16 @@ def get_link_txt(txt):
     raw_links = re.findall(r'<a href=".*?" title=".*?"', txt, re.S)
     links = {}
     for link in raw_links:
-        key = link[re.search(r'title=".*?"', link).span()[0]:re.search(r'title=".*?"', link).span()[1]]
-        key = key[re.search(r'".*?"', key).span()[0]:re.search(r'".*?"', key).span()[1]].strip('"')
-        links[key] = \
-            ("https://zh.minecraft.wiki" +
-             link[re.search(r'".*?"', link).span()[0]:re.search(r'".*?"', link).span()[1]].strip('"'))
+        key = re.findall(r'title="(.*?)"', link, re.M)[0]
+        ref = re.findall(r'href="(.*?)"', link, re.M)[0]
+        links[key] = "https://zh.minecraft.wiki" + ref
     return links
 
 
 def link_to_xaml(lk):
-    return f'''<Underline><local:MyTextButton EventType="打开网页" EventData="{lk[0]}" Margin="0,0,0,-8">{lk[1]}</local:MyTextButton></Underline>'''
+    xaml = f'''<Underline><local:MyTextButton EventType="打开网页" \
+    EventData="{lk[1]}" Margin="0,0,0,-8">{lk[0]}</local:MyTextButton></Underline>'''
+    return xaml
 
 
 def gs():
@@ -57,14 +56,8 @@ def update():
     with open("Custom.xaml", "r", encoding='UTF-8') as f:
         content_text = f.read()
     with open("Custom.xaml", "w", encoding='UTF-8') as f:
-        txt = re.sub(
-            r'''<Border Style="{StaticResource HeadImageBorder}">
-<Border.Background>
-<ImageBrush ImageSource=".*?" Stretch="UniformToFill"/>
-</Border.Background>
-<Image Source=".*?" Opacity="0" Stretch="Fill"/>
-</Border>''',
-            f'''<Border Style="{{StaticResource HeadImageBorder}}">
+        txt = re.sub(re.findall('''<Border.Background>\n<ImageBrush ImageSource="(.*?)" />\n</Border.Background><Image Source="(.*?)"/>\n</Border>''', content_text)[0],
+                     f'''<Border Style="{{StaticResource HeadImageBorder}}">
 <Border.Background>
 <ImageBrush ImageSource="{gs()}" Stretch="UniformToFill"/>
 </Border.Background>
@@ -75,16 +68,18 @@ def update():
         txt = re.sub(r'<sys:String x:Key="WikiPage">.*?</sys:String>', '<sys:String x:Key="WikiPage">' +
                      list(get_link_txt(f'''{obj.find('div', class_="weekly-content")}''').values())[0] +
                      "</sys:String>", txt)
-        txt = re.sub(r'<!-- NewsCard -->.*?<!-- end_NewsCard -->',
+        txt = re.sub(re.findall(r'<!-- NewsCard -->.*?<!-- end_NewsCard -->', txt, re.S)[0],
                      f'<!-- NewsCard -->{get_news_card()}<!-- end_NewsCard -->', txt, re.S)
-        txt = re.sub(r'<!-- introduction -->.*?<!-- end_introduction -->',
-                     f'<!-- introduction -->{gr()[0]}<!-- end_introduction -->', txt, re.S, re.M)
-        txt = re.sub(r'<!-- intro_2 -->.*?<!-- end_intro_2 -->',
+        txt = re.sub(r'^<!-- introduction -->(.*?)<!-- end_introduction -->',
+                     f'<!-- introduction -->{gr()[0]}<!-- end_introduction -->', txt, re.S)
+        txt = re.sub(r'^<!-- intro_2 -->(.*?)<!-- end_intro_2 -->',
                      f'<!-- intro_2 -->{gr()[1]}<!-- end_intro_2 -->', txt, re.S)
-        txt = re.sub(r'<!-- body -->.*?<!-- end_body -->',
+        txt = re.sub(r'^<!-- body -->(.*?)<!-- end_body -->',
                      f'<!-- body -->{"".join(gr()[2:])}<!-- end_body -->', txt, re.S)
-        print(txt)
-        f.write(txt)
+        if txt:
+            f.write(txt)
+        elif not txt:
+            print(txt)
 
 
 update()
