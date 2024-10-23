0670ae7eb84e704fa6ba66dfdad04afece306e42
仿生猫梦见苦力怕 <3372728115@qq.com>
2024-10-23 18:46:14 +0800

    fixed some bug :P

--- reloader.py
+++ reloader.py
@@ -213,7 +213,7 @@ M172.61,196.65h31a7.69,7.69,0,0,1,7.62,6.65l11.19,80.95c2.58,20.09,5.16,40.18,7.
     </Grid>
 </Border>
 </local:MyCard>
-<local:MyCard Title="中文 Minecraft Wiki 摘录--本周页面" CanSwap="True" IsSwaped="false" Style="{StaticResource Card}" >
+<local:MyCard Title="中文 Minecraft Wiki 摘录 - 本周页面" CanSwap="True" IsSwaped="false" Style="{StaticResource Card}" >
 <StackPanel Style="{StaticResource ContentStack}">
 <Border Style="{StaticResource HeadImageBorder}">
 <Border.Background>
@@ -222,7 +222,7 @@ M172.61,196.65h31a7.69,7.69,0,0,1,7.62,6.65l11.19,80.95c2.58,20.09,5.16,40.18,7.
 <Image Source="%(img)s" Opacity="0" Stretch="Fill"/>
 </Border>
 <Border Style="{StaticResource TitleBorder}">
-<TextBlock Style="{StaticResource TitleBlock}" Text="本周页面--%(topic)s" />
+<TextBlock Style="{StaticResource TitleBlock}" Text="%(topic)s" />
 </Border><FlowDocumentScrollViewer >
 <FlowDocument>
 <Paragraph Style="{StaticResource H2}">%(topic)s</Paragraph>
@@ -272,12 +272,11 @@ M172.61,196.65h31a7.69,7.69,0,0,1,7.62,6.65l11.19,80.95c2.58,20.09,5.16,40.18,7.
     </StackPanel>
     <StackPanel Orientation="Horizontal" VerticalAlignment="Center" Margin="-4,0,0,10" Height="56">
     <Image Margin="2" Source="http://creeperisaspy.tttttttttt.top/CardExt1nguisher.png"/>
-    <Image Margin="2" Source="https://image.stapxs.cn/i/2024/05/24/LB.png
-" />
+    <TextBlock Margin="0,2" Grid.Column="1" HorizontalAlignment="Left" FontSize="12" Foreground="{DynamicResource ColorBrush4}">主页使用了来自<Underline><local:MyTextButton EventType="打开网页" EventData="https://github.com/Light-Beacon" Foreground="{DynamicResource ColorBrush4}">@最亮的信标</local:MyTextButton></Underline>的模板。</TextBlock>
     </StackPanel>
     <StackPanel Orientation="Horizontal" VerticalAlignment="Center" Margin="-8,0,0,10">
-    <local:MyIconTextButton HorizontalAlignment="Left" Text="GitHub" ToolTip="前往Wiki 摘录主页 GitHub 页面" EventType="打开网页"
-    EventData="https://github.com/Ext1nguisher/PCLhomepage"
+    <local:MyIconTextButton HorizontalAlignment="Left" Text="Gitee" ToolTip="前往Wiki 摘录主页 Gitee 页面" EventType="打开网页"
+    EventData="https://gitee.com/planet_of_daniel/pcl2-homepage-fhg"
     LogoScale="1" Logo="M512 42.666667A464.64 464.64 0 0 0 42.666667 502.186667 460.373333 460.373333 0 0 0 363.52 938.666667c23.466667 4.266667 32-9.813333 32-22.186667v-78.08c-130.56 27.733333-158.293333-61.44-158.293333-61.44a122.026667 122.026667 0 0 0-52.053334-67.413333c-42.666667-28.16 3.413333-27.733333 3.413334-27.733334a98.56 98.56 0 0 1 71.68 47.36 101.12 101.12 0 0 0 136.533333 37.973334 99.413333 99.413333 0 0 1 29.866667-61.44c-104.106667-11.52-213.333333-50.773333-213.333334-226.986667a177.066667 177.066667 0 0 1 47.36-124.16 161.28 161.28 0 0 1 4.693334-121.173333s39.68-12.373333 128 46.933333a455.68 455.68 0 0 1 234.666666 0c89.6-59.306667 128-46.933333 128-46.933333a161.28 161.28 0 0 1 4.693334 121.173333A177.066667 177.066667 0 0 1 810.666667 477.866667c0 176.64-110.08 215.466667-213.333334 226.986666a106.666667 106.666667 0 0 1 32 85.333334v125.866666c0 14.933333 8.533333 26.88 32 22.186667A460.8 460.8 0 0 0 981.333333 502.186667 464.64 464.64 0 0 0 512 42.666667"/>
     <local:MyIconTextButton Text="CC BY-NC-SA 4.0" ToolTip="无特殊声明本主页文字内容使用该授权协议" EventType="打开网页"
     EventData="https://creativecommons.org/licenses/by-nc-sa/4.0/"
